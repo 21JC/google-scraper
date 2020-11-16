376228dffd908ad6c1be9427a2f7774ddb85d7f5
Pramurta Sinha | b3!ngD3v <contact.pycoder@gmail.com>
2020-11-17 00:22:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,73 +1,67 @@
 from selenium import webdriver
 import os
 import time
-from bs4 import BeautifulSoup as bs
 import json
+from bs4 import BeautifulSoup as bs
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-keywords=['mobile shops', 'tea shops']
-locations=['la', 'new york']
+keywords=['milk tea', 'bubble tea', '奶茶', '茶']
+locations=['new york city']
 
 max_data_keyword_per_location = 100
 
 for location in locations:
     for keyword in keywords:
+        max_data_keyword_per = max_data_keyword_per_location
         url=f"https://www.google.com/search?hl=en&tbm=lcl&q={keyword.replace(' ', '+')}+in+{location.replace(' ', '+')}&oq={keyword.replace(' ', '+')}+in+{location.replace(' ', '+')}"
 
         wcr_dict = os.getcwd() + '//chromedriver.exe'
         driver = webdriver.Chrome(executable_path=wcr_dict)
 
         driver.get(url)
-        while max_data_keyword_per_location > 0:
-            
+        while max_data_keyword_per > 0:
             time.sleep(5)
-            
             store_name = driver.find_elements_by_css_selector('.cXedhc.uQ4NLd')
             for x in range(0, len(store_name)-1):
-                a=store_name[x].get_attribute('innerHTML')
-                soup = bs(a, 'html.parser')
-                data_s=soup.get_text(separator='$').replace('\n', '').replace('  ', '').replace('   ', '').replace('$ ', '').replace('· ', '$').replace(' ⋅ ', '').replace('$Open', '$').replace('$$', '$').split('$')
-                if len(data_s) > 5 and len(data_s) < 7:
-                    name = data_s[0]
-                    ratings= data_s[1]
-                    number_ratings = data_s[2].replace('(', '').replace(')', '').replace(' ', '')
-                    store_type = data_s[3]
-                    address = data_s[4]
-                    closes = data_s[5].replace('Closes', '').replace(' ', '')
+                try:
+                    store_name[x].click()
                     try:
-                        mobile_no = data_s[6].replace(' ', '')
-                        dictionary ={ 
-                                "store name" : name, 
-                                "ratings" : ratings, 
-                                "rated by" : number_ratings, 
-                                "store type" : store_type,
-                                "address" : address,
-                                "closes" : closes,
-                                "mobile no." : mobile_no
-                        }
-                        
-                        json_object = json.dumps(dictionary, indent = 4) 
-                        
-                        with open(os.getcwd()+"//data.json", "a") as outfile: 
-                            outfile.write(json_object) 
+                        confirm = WebDriverWait(driver, 20).until(EC.presence_of_element_located(
+                            (By.CSS_SELECTOR, ".qrShPb.kno-ecr-pt.PZPZlf.mfMhoc.PPT5v span")))
+                    finally:
+                        pass
+                    time.sleep(6)
+                    name=driver.find_element_by_css_selector('.qrShPb.kno-ecr-pt.PZPZlf.mfMhoc.PPT5v span').text
+                    dictionary ={ 
+                            "store name" : name
+                    }
+                    try:
+                        el=driver.find_elements_by_class_name('YhemCb')
+                        dictionary["store type"]=el[len(el)-1].replace('"', '')
                     except:
-                        dictionary ={ 
-                                "store name" : name, 
-                                "ratings" : ratings, 
-                                "rated by" : number_ratings, 
-                                "store type" : store_type,
-                                "address" : address,
-                                "closes" : closes,
-                        }
-                        
-                        # Serializing json  
-                        json_object = json.dumps(dictionary, indent = 4)
-                        
-                        # Writing to sample.json 
-                        with open(os.getcwd()+"//data.json", "a") as outfile: 
-                            outfile.write(json_object) 
+                        pass
+                    try:
+                        dictionary["ratings"]=driver.find_element_by_css_selector('.Ob2kfd .Aq14fc').text
+                        dictionary["rated by"]=driver.find_element_by_css_selector('.hqzQac span a span').text
+                    except:
+                        pass
+                    
+                    a=driver.find_element_by_class_name('h2yBfgNjGpc__inline-item-view').get_attribute('innerHTML')
+                    soup = bs(a, 'html.parser')
+                    for info in soup.findAll("div", {"class": "zloOqf PZPZlf"}):
+                        dictionary[str(info.get_text()).split(": ")[0]]=str(info.get_text()).split(": ")[1]
 
-            max_data_keyword_per_location-= 20
-            if max_data_keyword_per_location > 0:
+                    json_object = json.dumps(dictionary, indent = 4) 
+                    
+                    with open(os.getcwd()+"//data.json", "a") as outfile: 
+                        outfile.write(json_object) 
+                except:
+                    pass
+            
+            max_data_keyword_per-= 20
+            if max_data_keyword_per > 0:
                 try:
                     driver.find_element_by_id('pnnext').click()
                 except:
